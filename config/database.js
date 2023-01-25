40b174197235a5144996fd75b4172892ca0d32d3
Vikram0369 <113485576+Vikram0369@users.noreply.github.com>
2023-01-24 23:45:08 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // url : "mongodb+srv://vikram369:vikram369@cluster0.av5mvv1.mongodb.net/user_info"
-    url : "mongodb+srv://vikram369:vikram369@cluster0.av5mvv1.mongodb.net/user_info"
+    //url: mongoose database url
 }
 
